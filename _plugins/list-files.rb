10cb0f95a91805980e98375d65f83cfa0bb9743e
Panagiotis Sarris <154690308+Panagiotissarr@users.noreply.github.com>
2025-12-04 12:05:25 +0200

    Update list-files.rb

--- _plugins/list-files.rb
+++ _plugins/list-files.rb
@@ -1,7 +1,4 @@
 # _plugins/list-files.rb
-# Generates site.data.file_index for all folders under /files
-# Includes folders as entries
-
 require 'pathname'
 
 module Jekyll
@@ -13,34 +10,31 @@ module Jekyll
       base_dir = File.join(site.source, "files")
       index = {}
 
-      if Dir.exist?(base_dir)
-        # Get all files and folders recursively
-        Dir.glob("#{base_dir}/**/*", File::FNM_DOTMATCH).each do |path|
-          next if path =~ /\/\.\.?$/ # skip . and ..
-          rel_path = Pathname.new(path).relative_path_from(Pathname.new(site.source)).to_s
-          folder = "/" + Pathname.new(path).dirname.relative_path_from(Pathname.new(site.source)).to_s + "/"
+      return unless Dir.exist?(base_dir)
 
-          index[folder] ||= []
+      Dir.glob("#{base_dir}/**/*").each do |path|
+        next if path =~ /\/\.\.?$/ # skip . and ..
+        rel_path = Pathname.new(path).relative_path_from(Pathname.new(site.source)).to_s
+        folder = "/" + Pathname.new(path).dirname.relative_path_from(Pathname.new(site.source)).to_s + "/"
 
-          if File.directory?(path)
-            # Add folder as an entry
-            index[folder] << {
-              "name" => File.basename(path),
-              "path" => "/" + rel_path + "/",
-              "size" => "-",
-              "date" => File.mtime(path),
-              "type" => "folder"
-            }
-          else
-            # Add file as an entry
-            index[folder] << {
-              "name" => File.basename(path),
-              "path" => "/" + rel_path,
-              "size" => "#{File.size(path)} B",
-              "date" => File.mtime(path),
-              "type" => "file"
-            }
-          end
+        index[folder] ||= []
+
+        if File.directory?(path)
+          index[folder] << {
+            "name" => File.basename(path),
+            "path" => "/" + rel_path + "/",
+            "size" => "-",
+            "date" => File.mtime(path),
+            "type" => "folder"
+          }
+        else
+          index[folder] << {
+            "name" => File.basename(path),
+            "path" => "/" + rel_path,
+            "size" => "#{File.size(path)} B",
+            "date" => File.mtime(path),
+            "type" => "file"
+          }
         end
       end
 
